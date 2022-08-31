726ba39e2782f577015704b17a7b412ba8b81ea4
ParkUWesleyJ <111292013+ParkUWesleyJ@users.noreply.github.com>
2022-08-31 15:14:06 -0500

    Quality Index added, other optimizations
    - Added the Quality Index algorithm. Another measurement that shows how distorted an image is.
    - Added header comment
    - MSE calculation runs significantly faster through implementation of NumPy
    - Output formatting changed

--- src/StegEval.py
+++ src/StegEval.py
@@ -1,37 +1,25 @@
+#########################################################
+# StegEval v0.2
+# Wesley Jacobs
+#
+# A program consisting of several algorithms used to
+# guage distortion levels and security of images before
+# and after image hiding.
+#########################################################
+
 from json.encoder import INFINITY # used for case where PSNR is undefined
 import math                       # used for log10
+import numpy as np                # used to calculate averages and sums of Image pixels more efficiently
 from PIL import Image             # used for image processing
 import sys                        # used to exit on error/quit
 
 #########################################################
 
 # Calculates Mean Square Error (MSE)
-def calcMSE(imO, imS, pxO, pxS):
-    totalRed = 0.0
-    totalGreen = 0.0
-    totalBlue = 0.0
-    curLoop = 1
-    lastPrinted = 0
+def calcMSE(imO, imS):
+    mse = np.square(np.subtract(imO, imS)).mean()
 
-    for col in range(0, imO.size[1]):
-        for row in range(0, imO.size[0]):
-            totalRed += (pxO[row, col][0] - pxS[row, col][0]) ** 2
-            totalGreen += (pxO[row, col][1] - pxS[row, col][1]) ** 2
-            totalGreen += (pxO[row, col][2] - pxS[row, col][2]) ** 2
-
-            curLoop += 1
-            curVal = round(curLoop / (imO.size[0]*imO.size[1]) * 100)
-            if ((curVal * 10) != lastPrinted and (curVal * 10) % 10 == 0):
-                lastPrinted = (curVal * 10)
-                print("MSE | Progress: " + (str) (curVal) + "%", end='\r')
-                
-    
-    mse = (totalRed+totalGreen+totalBlue) / (3*imO.size[1]*imO.size[0])
-
-    print(end="\x1b[2K")
-    print("MSE |", mse, "(Lower = Better)")
-
-    return mse
+    return round(mse, 10)
 
 #########################################################
 
@@ -42,8 +30,24 @@ def calcPSNR(mse):
     else:
         psnr = 10 * math.log10((255 ** 2) / mse)
 
-    print("PSNR |", psnr, "(Higher = Better)")
-    return psnr
+    return round(psnr, 10)
+
+#########################################################
+
+# Calculates Quality Index (QI)
+def calcQI(imO, imS):
+    avgO = np.mean(imO)
+    avgS = np.mean(imS)
+    stdDevO = np.sum(np.subtract(imS, avgS) ** 2) / (3*imO.size[0]*imO.size[1]-1)
+    stdDevS = np.sum(np.subtract(imO, avgO) ** 2) / (3*imO.size[0]*imO.size[1]-1)
+    covariance = np.sum(np.subtract(imO, avgO) * np.subtract(imS, avgS)) / (3*imO.size[0]*imO.size[1]-1)
+
+    if (stdDevO == 0 and stdDevS == 0):
+        qi = 1
+    else:
+        qi = (4 * covariance * avgO * avgS) / ((stdDevO + stdDevS) * ((avgO ** 2) + (avgS ** 2)))
+
+    return round(qi, 10)
 
 #########################################################
 
@@ -53,6 +57,7 @@ imagePathStego = input("Enter path of stego image: ")
 
 mse = 0
 psnr = 0
+qi = 0
 
 try:
     with Image.open(imagePathOriginal) as imO:
@@ -67,8 +72,15 @@ if (imO.size != imS.size):
     print("The two provided images are not the same size.")
     sys.exit()
 
-print("\n--==RESULTS==--")
-mse = calcMSE(imO, imS, pxO, pxS)
+mse = calcMSE(imO, imS)
 psnr = calcPSNR(mse)
+qi = calcQI(imO, imS)
+
+print("\n--==DISTORTION MEASUREMENTS==--")
+print("{:<5} | {:<20} | Lower = Better".format("MSE", mse))
+print("{:<5} | {:<20} | Higher = Better".format("PSNR", psnr))
+print("{:<5} | {:<20} | Higher = Better".format("QI", qi))
+
+print("\n--==SECURITY MEASUREMENTS==--")
 
 # END
