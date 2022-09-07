12f45768c521e9b4a728054841fd6c566f0d866a
ParkUWesleyJ <111292013+ParkUWesleyJ@users.noreply.github.com>
2022-09-07 14:18:55 -0500

    StegEval v0.3
    Added SSIM to distortion measurements, also added more comments to describe the calculations

--- src/StegEval.py
+++ src/StegEval.py
@@ -1,5 +1,5 @@
 #########################################################
-# StegEval v0.2
+# StegEval v0.3
 # Wesley Jacobs
 #
 # A program consisting of several algorithms used to
@@ -12,12 +12,14 @@ import math                       # used for log10
 import numpy as np                # used to calculate averages and sums of Image pixels more efficiently
 from PIL import Image             # used for image processing
 import sys                        # used to exit on error/quit
+np.set_printoptions(threshold=sys.maxsize)
 
 #########################################################
 
 # Calculates Mean Square Error (MSE)
 def calcMSE(imO, imS):
-    mse = np.square(np.subtract(imO, imS)).mean()
+    # get the squared difference between all pixels then get the average
+    mse = np.mean(np.square(np.subtract(imO, imS)))
 
     return round(mse, 10)
 
@@ -36,14 +38,19 @@ def calcPSNR(mse):
 
 # Calculates Quality Index (QI)
 def calcQI(imO, imS):
+    # average pixel value in original image
     avgO = np.mean(imO)
+    # average pixel value in stego-image
     avgS = np.mean(imS)
+    # standard deviation of pixel values in original image
     stdDevO = np.sum(np.subtract(imS, avgS) ** 2) / (3*imO.size[0]*imO.size[1]-1)
+    # standard deviation of pixel values in stego image
     stdDevS = np.sum(np.subtract(imO, avgO) ** 2) / (3*imO.size[0]*imO.size[1]-1)
+    # covariance of both original and stego image pixel values
     covariance = np.sum(np.subtract(imO, avgO) * np.subtract(imS, avgS)) / (3*imO.size[0]*imO.size[1]-1)
 
     if (stdDevO == 0 and stdDevS == 0):
-        qi = 1
+        qi = 1.0
     else:
         qi = (4 * covariance * avgO * avgS) / ((stdDevO + stdDevS) * ((avgO ** 2) + (avgS ** 2)))
 
@@ -51,13 +58,34 @@ def calcQI(imO, imS):
 
 #########################################################
 
+# Calculates Structural Similary Index Measure (SSIM)
+# WARNING: Will likely yield poor results as it's better suited for grayscale images
+def calcSSIM(imO, imS):
+    # average pixel value in original image
+    avgO = np.mean(imO)
+    # average pixel value in stego-image
+    avgS = np.mean(imS)
+    # standard deviation of pixel values in original image
+    stdDevO = np.sum(np.subtract(imS, avgS) ** 2) / (3*imO.size[0]*imO.size[1]-1)
+    # standard deviation of pixel values in stego image
+    stdDevS = np.sum(np.subtract(imO, avgO) ** 2) / (3*imO.size[0]*imO.size[1]-1)
+    # covariance of both original and stego image pixel values
+    covariance = np.sum(np.subtract(imO, avgO) * np.subtract(imS, avgS)) / (3*imO.size[0]*imO.size[1]-1)
+    # first stablizier, takes a constant (.01 in this case) then multiplies it by 2^(bits per pixel), subtracts one, then squares it
+    stabilizer1 = (.01 * (2 ** (8 * 3) - 1)) ** 2
+    # first stablizier, takes a constant (.03 in this case) then multiplies it by 2^(bits per pixel), subtracts one, then squares it
+    stabilizer2 = (.03 * (2 ** (8 * 3) - 1)) ** 2
+
+    ssim = ((2 * avgO * avgS + stabilizer1) * (2 * covariance + stabilizer2)) / ((avgO ** 2 + avgS ** 2 + stabilizer1) * (stdDevO + stdDevS + stabilizer2))
+    print(ssim)
+
+    return round(ssim, 10)
+
+#########################################################
+
 # DRIVER CODE
 imagePathOriginal = input("Enter path of original image: ")
 imagePathStego = input("Enter path of stego image: ")
-
-mse = 0
-psnr = 0
-qi = 0
 
 try:
     with Image.open(imagePathOriginal) as imO:
@@ -75,11 +103,14 @@ if (imO.size != imS.size):
 mse = calcMSE(imO, imS)
 psnr = calcPSNR(mse)
 qi = calcQI(imO, imS)
+ssim = calcSSIM(imO, imS)
+
 
 print("\n--==DISTORTION MEASUREMENTS==--")
 print("{:<5} | {:<20} | Lower = Better".format("MSE", mse))
 print("{:<5} | {:<20} | Higher = Better".format("PSNR", psnr))
 print("{:<5} | {:<20} | Higher = Better".format("QI", qi))
+print("{:<5} | {:<20} | Higher = Better".format("SSIM", ssim))
 
 print("\n--==SECURITY MEASUREMENTS==--")
 
