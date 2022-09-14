afe8637a4987d68f8d96b5ec1118ed3a0d1d587d
ParkUWesleyJ <111292013+ParkUWesleyJ@users.noreply.github.com>
2022-09-14 11:20:18 -0500

    StegEval v0.4
    Removed SSIM measurement as the measurements were similar to QI. Added Pixel Value Difference Histogram to detect PVD steganography method. Before running the program, it now checks if the modules needed exist.

--- src/StegEval.py
+++ src/StegEval.py
@@ -1,5 +1,5 @@
 #########################################################
-# StegEval v0.3
+# StegEval v0.4
 # Wesley Jacobs
 #
 # A program consisting of several algorithms used to
@@ -7,12 +7,37 @@
 # and after image hiding.
 #########################################################
 
-from json.encoder import INFINITY # used for case where PSNR is undefined
-import math                       # used for log10
-import numpy as np                # used to calculate averages and sums of Image pixels more efficiently
-from PIL import Image             # used for image processing
-import sys                        # used to exit on error/quit
-np.set_printoptions(threshold=sys.maxsize)
+import sys                            # used to exit on error/quit
+
+try:
+    from json.encoder import INFINITY # used for case where PSNR is undefined
+except:
+    print("Missing json.encode module")
+    sys.exit()
+
+try:
+    import math                       # used for log10
+except:
+    print("Missing math module")
+    sys.exit()
+
+try:
+    import numpy as np                # used to calculate averages and sums of Image pixels more efficiently
+except:
+    print("Missing NumPy module")
+    sys.exit()
+
+try:
+    from PIL import Image             # used for image processing
+except:
+    print("Missing Pillow module")
+    sys.exit()
+
+try:
+    import matplotlib.pyplot as plt   # used to visually represent histograms
+except:
+    print("Missing matplotlib module")
+    sys.exit()
 
 #########################################################
 
@@ -58,30 +83,39 @@ def calcQI(imO, imS):
 
 #########################################################
 
-# Calculates Structural Similary Index Measure (SSIM)
-# WARNING: Will likely yield poor results as it's better suited for grayscale images
-def calcSSIM(imO, imS):
-    # average pixel value in original image
-    avgO = np.mean(imO)
-    # average pixel value in stego-image
-    avgS = np.mean(imS)
-    # standard deviation of pixel values in original image
-    stdDevO = np.sum(np.subtract(imS, avgS) ** 2) / (3*imO.size[0]*imO.size[1]-1)
-    # standard deviation of pixel values in stego image
-    stdDevS = np.sum(np.subtract(imO, avgO) ** 2) / (3*imO.size[0]*imO.size[1]-1)
-    # covariance of both original and stego image pixel values
-    covariance = np.sum(np.subtract(imO, avgO) * np.subtract(imS, avgS)) / (3*imO.size[0]*imO.size[1]-1)
-    # first stablizier, takes a constant (.01 in this case) then multiplies it by 2^(bits per pixel), subtracts one, then squares it
-    stabilizer1 = (.01 * (2 ** (8 * 3) - 1)) ** 2
-    # first stablizier, takes a constant (.03 in this case) then multiplies it by 2^(bits per pixel), subtracts one, then squares it
-    stabilizer2 = (.03 * (2 ** (8 * 3) - 1)) ** 2
+# Gets Pixel Value Difference Histogram
+def showHist(imO, imS):
+    # reshapes original image array to subtract pairs of pixel avalues
+    reshapedO = np.array(imO, dtype=np.int16).reshape(2, np.array(imO).size // 2)
+    # array of difference between pairs of pixel values in original image
+    pixelDiffO = np.subtract(reshapedO[0], reshapedO[1])
+    # reshapes stego image array to subtract pairs of pixel avalues
+    reshapedS = np.array(imS, dtype=np.int16).reshape(2, np.array(imS).size // 2)
+    # array of difference between pairs of pixel values in stego image
+    pixelDiffS = np.subtract(reshapedS[0], reshapedS[1])
 
-    ssim = ((2 * avgO * avgS + stabilizer1) * (2 * covariance + stabilizer2)) / ((avgO ** 2 + avgS ** 2 + stabilizer1) * (stdDevO + stdDevS + stabilizer2))
-    print(ssim)
+    # average of pixel value difference in original image
+    pixelDiffAvgO = np.mean(pixelDiffO)
+    # standard deviation of pixel value difference in original image
+    pixelDiffStdO = np.std(pixelDiffO)
+    # average of pixel value difference in stego image
+    pixelDiffAvgS = np.mean(pixelDiffS)
+    # standard deviation of pixel value difference in stego image
+    pixelDiffStdS = np.std(pixelDiffS)
 
-    return round(ssim, 10)
+    plt.hist(x=pixelDiffO, bins=range(pixelDiffO.min(), pixelDiffO.max() + 1), color="#000000", alpha=0.5, histtype="step", label="Original")
+    plt.hist(x=pixelDiffS, bins=range(pixelDiffS.min(), pixelDiffS.max() + 1), color="#ff1100", alpha=0.5, histtype="step", label="Stego")
 
-#########################################################
+    plt.xlabel("Pixel Difference")
+    plt.ylabel("Occurences")
+    plt.title("Pixel Value Difference Histogram")
+    plt.legend()
+
+    plt.tight_layout()
+
+    plt.show()
+
+    return [pixelDiffAvgO, pixelDiffStdO, pixelDiffAvgS, pixelDiffStdS]
 
 # DRIVER CODE
 imagePathOriginal = input("Enter path of original image: ")
@@ -103,15 +137,20 @@ if (imO.size != imS.size):
 mse = calcMSE(imO, imS)
 psnr = calcPSNR(mse)
 qi = calcQI(imO, imS)
-ssim = calcSSIM(imO, imS)
-
 
 print("\n--==DISTORTION MEASUREMENTS==--")
 print("{:<5} | {:<20} | Lower = Better".format("MSE", mse))
 print("{:<5} | {:<20} | Higher = Better".format("PSNR", psnr))
 print("{:<5} | {:<20} | Higher = Better".format("QI", qi))
-print("{:<5} | {:<20} | Higher = Better".format("SSIM", ssim))
 
-print("\n--==SECURITY MEASUREMENTS==--")
+histInfo = showHist(imO, imS)
+
+print("\n--==PIXEL VALUE DIFFERENCE HISTOGRAM INFO==--")
+print("{:<20} | {:<20}".format("Mean Original", histInfo[0]))
+print("{:<20} | {:<20}".format("StdDev Original", histInfo[1]))
+print("{:<20} | {:<20}".format("Mean Stego", histInfo[2]))
+print("{:<20} | {:<20}".format("StdDev Stego", histInfo[3]))
+
+print("\n--==RS ANALYSIS INFO==--")
 
 # END
