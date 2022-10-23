7e21012918f4e85b5e4565f41ce69a65e003d8f2
ParkUWesleyJ <111292013+ParkUWesleyJ@users.noreply.github.com>
2022-10-23 12:23:17 -0500

    Add files via upload

--- src/StegEval.py
+++ src/StegEval.py
@@ -23,6 +23,9 @@ except:
 #########################################################
 
 # Calculates Mean Square Error (MSE)
+
+#########################################################
+
 def calc_mse(img_original, img_stego):
     # Get the squared difference between all pixels then get the average
     mse = np.mean(np.square(np.subtract(img_original, img_stego, dtype=np.int32)))
@@ -32,6 +35,9 @@ def calc_mse(img_original, img_stego):
 #########################################################
 
 # Calculates Peak Signal-to-Noise Ratio (PSNR)
+
+#########################################################
+
 def calc_psnr(mse):
     if (mse == 0):
         psnr = INFINITY
@@ -43,6 +49,9 @@ def calc_psnr(mse):
 #########################################################
 
 # Calculates Quality Index (QI)
+
+#########################################################
+
 def calc_qi(img_original, img_stego):
     # Average pixel values
     avg_original = np.mean(img_original)
@@ -65,16 +74,19 @@ def calc_qi(img_original, img_stego):
 #########################################################
 
 # Gets Pixel Value Difference Histogram
+
+#########################################################
+
 def show_hist(img_original, img_stego):
-    # gets an array of individual pixel average values for original image
+    # Gets an array of individual pixel average values
     avg_pix_array_original = np.mean(np.array(img_original, dtype=np.int32).reshape(np.array(img_original).size // 3, 3), axis=1)
-    # gets an array of individual pixel average values for stego image
     avg_pix_array_stego = np.mean(np.array(img_stego, dtype=np.int32).reshape(np.array(img_stego).size // 3, 3), axis=1)
 
+    # Create histograms
     y, x, _ = plt.hist(x=avg_pix_array_original, bins=range(0, 256), color="#000000", alpha=0.5, histtype="step", label="Original")
     plt.hist(x=avg_pix_array_stego, bins=range(0, 256), color="#ff0000", alpha=0.5, histtype="step", label="Stego")
 
-    # several values that can be used to see differences between images
+    # Several values that can be used to see differences between images
     avg_original = np.mean(avg_pix_array_original)
     avg_stego = np.mean(avg_pix_array_stego)
     std_original = np.std(avg_pix_array_original)
@@ -82,12 +94,12 @@ def show_hist(img_original, img_stego):
     minmax_original = (np.max(avg_pix_array_original), np.min(avg_pix_array_original))
     minmax_stego = (np.max(avg_pix_array_stego), np.min(avg_pix_array_stego))
 
+    # Histogram formatting
     plt.ylim([0, y.max()+1])
     plt.xlabel("Pixel values")
     plt.ylabel("Occurences")
     plt.title("Pixel Value Histogram")
     plt.legend()
-
     plt.tight_layout()
 
     plt.show()
@@ -100,64 +112,68 @@ def show_hist(img_original, img_stego):
 
 #########################################################
 
-# Gets original image
-original_path = input("Enter path of original image (Q to quit): ")
+def main ():
+    # Gets original image
+    original_path = input("Enter path of original image (Q to quit): ")
 
-while (not os.path.isfile(original_path) and original_path.lower() != "q"):
-    original_path = input("Invalid path. Enter path of original image (Q to quit): ")
+    while (not os.path.isfile(original_path) and original_path.lower() != "q"):
+        original_path = input("Invalid path. Enter path of original image (Q to quit): ")
 
-if (original_path.lower() == "q"):
-    sys.exit()
+    if (original_path.lower() == "q"):
+        sys.exit()
 
-# Tries to open image into RGB mode
-try:
-    with Image.open(original_path).convert("RGB") as img_original:
-        pixels_original = img_original.load()
-        print("Image dimensions: " + str(img_original.size[0]) + "x" + str(img_original.size[1]))
-except:
-    print("Unable to get image into correct format. (Only works with RGB images)")
-    sys.exit()
+    # Tries to open image into RGB mode
+    try:
+        with Image.open(original_path).convert("RGB") as img_original:
+            pixels_original = img_original.load()
+            print("Image dimensions: " + str(img_original.size[0]) + "x" + str(img_original.size[1]))
+    except:
+        print("Unable to get image into correct format. (Only works with RGB images)")
+        sys.exit()
 
-# Gets stego image
-stego_path = input("Enter path of stego image (Q to quit): ")
+    # Gets stego image
+    stego_path = input("Enter path of stego image (Q to quit): ")
 
-while (not os.path.isfile(stego_path) and stego_path.lower() != "q"):
-    stego_path = input("Invalid path. Enter path of stego image (Q to quit): ")
+    while (not os.path.isfile(stego_path) and stego_path.lower() != "q"):
+        stego_path = input("Invalid path. Enter path of stego image (Q to quit): ")
 
-if (stego_path.lower() == "q"):
-    sys.exit()
+    if (stego_path.lower() == "q"):
+        sys.exit()
 
-# Tries to open image into RGB mode
-try:
-    with Image.open(stego_path).convert("RGB") as img_stego:
-        img_stego.load()
-        print("Image dimensions: " + str(img_stego.size[0]) + "x" + str(img_stego.size[1]))
-except:
-    print("Unable to get image into correct format. (Only works with RGB images)")
-    sys.exit()
+    # Tries to open image into RGB mode
+    try:
+        with Image.open(stego_path).convert("RGB") as img_stego:
+            img_stego.load()
+            print("Image dimensions: " + str(img_stego.size[0]) + "x" + str(img_stego.size[1]))
+    except:
+        print("Unable to get image into correct format. (Only works with RGB images)")
+        sys.exit()
 
-# Stops program if the images aren't the same size
-if (img_original.size != img_stego.size):
-    print("The two provided images are not the same size!")
-    sys.exit()
+    # Stops program if the images aren't the same size
+    if (img_original.size != img_stego.size):
+        print("The two provided images are not the same size!")
+        sys.exit()
 
-# Calculates MSE, PSNR, and QI
-mse = calc_mse(img_original, img_stego)
-psnr = calc_psnr(mse)
-qi = calc_qi(img_original, img_stego)
+    # Calculates MSE, PSNR, and QI
+    mse = calc_mse(img_original, img_stego)
+    psnr = calc_psnr(mse)
+    qi = calc_qi(img_original, img_stego)
 
-print("\n--==DISTORTION MEASUREMENTS==--")
-print("{:<5} | {:<20} | Lower = Better".format("MSE", mse))
-print("{:<5} | {:<20} | Higher = Better".format("PSNR", psnr))
-print("{:<5} | {:<20} | Higher = Better".format("QI", "INVALID (STDDEV=0)" if qi == INFINITY else qi))
+    print("\n--==DISTORTION MEASUREMENTS==--")
+    print("{:<5} | {:<20} | Lower = Better".format("MSE", mse))
+    print("{:<5} | {:<20} | Higher = Better".format("PSNR", psnr))
+    print("{:<5} | {:<20} | Higher = Better".format("QI", "INVALID (STDDEV=0)" if qi == INFINITY else qi))
 
-# Shows histogram and gets statistics of each image
-hist_info = show_hist(img_original, img_stego)
+    # Shows histogram and gets statistics of each image
+    hist_info = show_hist(img_original, img_stego)
 
-print("\n--==HISTOGRAM INFORMATION==--")
-print("{:<20} | {:<20}".format("Mean Original", hist_info[0]))
-print("{:<20} | {:<20}".format("Mean Stego", hist_info[1]))
-print("{:<20} | {:<20}".format("StdDev Original", hist_info[2]))
-print("{:<20} | {:<20}".format("StdDev Stego", hist_info[3]))
-print("{:<20} | {:<20}".format("MinMax Original", str(hist_info[4])))
-print("{:<20} | {:<20}".format("MinMax Stego", str(hist_info[5])))
+    print("\n--==HISTOGRAM INFORMATION==--")
+    print("{:<20} | {:<20}".format("Mean Original", hist_info[0]))
+    print("{:<20} | {:<20}".format("Mean Stego", hist_info[1]))
+    print("{:<20} | {:<20}".format("StdDev Original", hist_info[2]))
+    print("{:<20} | {:<20}".format("StdDev Stego", hist_info[3]))
+    print("{:<20} | {:<20}".format("MinMax Original", str(hist_info[4])))
+    print("{:<20} | {:<20}".format("MinMax Stego", str(hist_info[5])))
+
+if __name__ == "__main__":
+    sys.exit(main())
--- src/UniSteg.py
+++ src/UniSteg.py
@@ -1,41 +1,39 @@
 #########################################################
-# UniSteg v0.1
+# UniSteg v0.1.1
 # Wesley Jacobs
 #
 # Currently an exploratory program that conceals and
 # extracts message to and from images.
 #########################################################
 
-from PIL import Image # used for image processing
 import sys            # used to exit on error/quit
+import os.path        # used to check if file exists
 
-processingType = input("[C]onceal | [E]xtract | [Q]uit\n").lower()
-while (processingType != "c" and processingType != "e" and processingType != "q"):
-    processingType = input("[C]onceal | [E]xtract | [Q]uit\n").lower()
-
-# ON QUIT
-if (processingType == "q"):
+try:
+    from PIL import Image # used for image processing
+except:
+    print("Missing a module. Did you run 'pip install -r modules.txt'?")
     sys.exit()
-# ON CONCEAL
-elif (processingType == "c"):
-    imagePath = input("Enter path of image: ")
 
-    try:
-        with Image.open(imagePath) as im:
-            px = im.load()
-    except:
-        print("Image not found.")
-        sys.exit()
+#########################################################
 
+# Conceals a secret message into an image and saves it in current directory
+
+#########################################################
+
+def conceal(im, px):
     secretMessageString = input("Enter your secret message: ")
 
+    # convert string into an array of ASCII values
     secretMessageByteArray = bytearray(secretMessageString, "ascii")
-    secretMessageBinary = ""
+    secretMessageBinary = "" # used to store the secret message in binary format
 
+    # loops through each ascii value
     for byte in secretMessageByteArray:
-        # Adds leading zeros
+        # Adds leading zeros so each character is guaranteed to be 8 bits in length [2:] gets rid of the '0b' before the binary value
         for bit in range(8-len(str(bin(byte)[2:]))):
             secretMessageBinary += "0"
+        # Adds the byte of data to the secret message binary string
         secretMessageBinary += str(bin(byte)[2:])
 
     # Forces the greatest significant bit of the next byte to be a one, which makes anything after the secret message not show up when extracted
@@ -55,6 +53,7 @@ elif (processingType == "c"):
             
             count = 0
             
+            # Goes through color value and makes it even if it's supposed to be a 0 and odd if it's supposed to be a 1
             while (count < 3 and messagePosition != len(secretMessageBinary)):
                 if count == 0 and int(secretMessageBinary[messagePosition]) != r % 2:
                     if (r-1 > 0):
@@ -79,18 +78,17 @@ elif (processingType == "c"):
 
     im.save('stego.png')
     print("Successfully created stegimage.")
-# ON EXTRACT
-elif (processingType == "e"):
-    imagePath = input("Enter path of image: ")
-    binaryMessage = ""
-    curBit = 0
 
-    try:
-        with Image.open(imagePath) as im:
-            px = im.load()
-    except:
-        print("Image not found.")
-        sys.exit()
+#########################################################
+
+# Extracts secret message from image and outputs it to console
+
+#########################################################
+
+def extract(im, px):
+    # Note: THIS CAN BE OPTIMIZED BY TERMINATING THE READ EARLY WHEN IT HITS THE TERMINATING SET OF BINARY VALUES
+
+    binaryMessage = ""
 
     # Creates a string of every least significant bit in every color of every pixel
     for col in range(0, im.size[1]):
@@ -98,13 +96,14 @@ elif (processingType == "e"):
             for color in range(0, 3):
                 binaryMessage += str(px[row, col][color]%2)
 
-    # Converts string into a format that python can decode
+    # Converts string into a format that can be decoded
     binaryMessage = int(binaryMessage, 2)
+    # Converts the formatted string into ASCII values (unicode values if it doesn't fit in ASCII range)
     binaryArray = binaryMessage.to_bytes((binaryMessage.bit_length() + 7) // 8, "big")
 
     lastValidCharacter = 0
 
-    # Gets the last character that can be represented using ASCII encoding
+    # Gets the last character that can be represented using ASCII encoding, which should be the end of the message
     for element in binaryArray:
         if (element < 128):
             lastValidCharacter += 1
@@ -113,4 +112,43 @@ elif (processingType == "e"):
 
     print("Decoded message: \"" + binaryArray[0:lastValidCharacter].decode("ascii") + "\"")
 
-# END
+#########################################################
+
+# DRIVER CODE
+
+#########################################################
+
+def main():
+    # Gets the type of processing to conduct on the image
+    processingType = input("[C]onceal | [E]xtract | [Q]uit\n").lower()
+
+    while (processingType != "c" and processingType != "e" and processingType != "q"):
+        processingType = input("[C]onceal | [E]xtract | [Q]uit\n").lower()
+
+    # Gets the path of the image to process
+    imagePath = input("Enter path of image: ")
+
+    while (not os.path.isfile(imagePath) and imagePath.lower() != "q"):
+        imagePath = input("Invalid path. Enter path of image: ")
+
+    # ON QUIT
+    if (processingType == "q"):
+        sys.exit()
+
+    # Attempts to open the image from the given path and get the pixel data
+    try:
+        with Image.open(imagePath) as im:
+            px = im.load()
+    except:
+        print("Image not found.")
+        sys.exit()
+
+    # Conceal
+    if (processingType == "c"):
+        conceal(im, px)
+    # Extract
+    elif (processingType == "e"):
+        extract(im, px)
+
+if __name__ == "__main__":
+    sys.exit(main())
