3b8ba56c180809d3cc1bd21f2d155f55e4c5ad06
ParkUWesleyJ <111292013+ParkUWesleyJ@users.noreply.github.com>
2022-11-20 19:14:16 -0600

    UniSteg v0.3 and Other Changes
    UniSteg v0.3 supports utf-8 encoding.
    Other changes
    - Variables names consistently in snake_case
    - Fixed a bug with image input that would give an error when trying to quit
    - Changed how UniSteg and StegEval instances are initialized

--- src/Driver.py
+++ src/Driver.py
@@ -2,7 +2,7 @@
 # Driver.py v0.1
 # Wesley Jacobs
 #
-# Drives thje UniSteg and StegEval algorithms to provide
+# Drives the UniSteg and StegEval algorithms to provide
 # a pleasant user experience. Can be altered as the
 # UniSteg and StegEval classes validate image inputs.
 #########################################################
@@ -11,12 +11,12 @@ from UniSteg import UniSteg
 from StegEval import StegEval
 
 import os.path
-from json.encoder import INFINITY # used for case where PSNR is undefined
+from json.encoder import INFINITY  # used for case where PSNR is undefined
 import sys
 
 try:
-    from PIL import Image # used for image processing
-except:
+    from PIL import Image  # used for image processing
+except ModuleNotFoundError:
     print("Missing a module. Did you run 'pip install -r modules.txt'?")
     sys.exit()
 
@@ -25,6 +25,7 @@ colors = {
     "invalid": "\033[91m",
     "normal": "\033[0m"
 }
+
 
 # The main code to drive the two algorithms
 def main():
@@ -38,7 +39,7 @@ def main():
     ).lower()
 
     # Input validation for algorithm type
-    while (algorithm_type != "/evaluate" and algorithm_type != "/conceal_extract" and algorithm_type != "/quit"):
+    while algorithm_type != "/evaluate" and algorithm_type != "/conceal_extract" and algorithm_type != "/quit":
         algorithm_type = input(
             colors["invalid"] + "Invalid command.\n" + colors["normal"] +
             "\n---------------------------------------\n" +
@@ -47,27 +48,31 @@ def main():
             "---------------------------------------\n\n" +
             "> "
         ).lower()
-    
+
     # Handling and setting algorithm type
-    if (algorithm_type == "/conceal_extract"):
+    if algorithm_type == "/conceal_extract":
         algorithm = UniSteg()
-    elif (algorithm_type == "/evaluate"):
+    elif algorithm_type == "/evaluate":
         algorithm = StegEval()
-    elif (algorithm_type == "/quit"):
+    else:
         print("Quitting...")
         return
 
     # Handles when user wants to conceal/decode
-    if (type(algorithm) == UniSteg):
+    if isinstance(algorithm, UniSteg):
         # Gets one image from user input
         image = input_image(1, [])
+
+        # Handles when user quits
+        if image is None:
+            return
 
         # Sets the image for the UniSteg object
         algorithm.set_image(image[0])
 
         # Gets the type of processing to conduct on the image
-        processing_type = input(     
-            "\n--------------------------------------\n" +       
+        processing_type = input(
+            "\n--------------------------------------\n" +
             "Enter one of the following commands:\n" +
             " /conceal\n /extract\n /home\n /quit\n" +
             "---------------------------------------\n\n" +
@@ -75,10 +80,11 @@ def main():
         ).lower()
 
         # Input validation for processing method
-        while (processing_type != "/conceal" and processing_type != "/extract" and processing_type != "/home" and processing_type != "/quit"):
-            processing_type = input(    
-                colors["invalid"] + "Invalid command.\n" + colors["normal"] + 
-                "\n---------------------------------------\n" +       
+        while processing_type != "/conceal" and processing_type != "/extract" \
+                and processing_type != "/home" and processing_type != "/quit":
+            processing_type = input(
+                colors["invalid"] + "Invalid command.\n" + colors["normal"] +
+                "\n---------------------------------------\n" +
                 "Enter one of the following commands:\n" +
                 " /conceal\n /extract\n /home\n /quit\n" +
                 "---------------------------------------\n\n" +
@@ -86,27 +92,32 @@ def main():
             ).lower()
 
         # Handles processing type, quitting, and going back to home
-        if (processing_type == "/conceal"):
+        if processing_type == "/conceal":
             algorithm.conceal()
-        elif (processing_type == "/extract"):
-            algorithm.extract()
-        elif (processing_type == "/home"):
+            print("Steg-image created.")
+        elif processing_type == "/extract":
+            print("Extracted message:", algorithm.extract())
+        elif processing_type == "/home":
             print("Returning to home.\n")
             main()
-        elif (processing_type == "/quit"):
+        elif processing_type == "/quit":
             print("Quitting...")
             return
     # Handles when user wants to evaluate
-    elif (type(algorithm) == StegEval):
+    elif isinstance(algorithm, StegEval):
         # Gets two images from user input
         images = input_image(2, [])
 
+        # Handles when user quits
+        if images is None or images[0] is None or images[1] is None:
+            return
+
         # Sets the images for the StegEval object
         algorithm.set_images(images)
-        
+
         # Gets the method of evaluation to run on the images
-        eval_type = input(     
-            "\n--------------------------------------\n" +       
+        eval_type = input(
+            "\n--------------------------------------\n" +
             "Enter one of the following commands:\n" +
             " /mse\n /psnr\n /qi\n /hist\n /all_evals\n /home\n /quit\n" +
             "---------------------------------------\n\n" +
@@ -114,11 +125,12 @@ def main():
         ).lower()
 
         # Input validation for method of evaluation
-        while (eval_type != "/mse" and eval_type != "/psnr" and eval_type != "/qi" 
-               and eval_type != "/hist" and eval_type != "/all_evals" and eval_type != "/home" and eval_type != "/quit"):
-            eval_type = input(    
-                colors["invalid"] + "Invalid input.\n" + colors["normal"] + 
-                "\n--------------------------------------\n" +       
+        while eval_type != "/mse" and eval_type != "/psnr" and eval_type != "/qi" \
+                and eval_type != "/hist" and eval_type != "/all_evals" and eval_type != "/home" \
+                and eval_type != "/quit":
+            eval_type = input(
+                colors["invalid"] + "Invalid input.\n" + colors["normal"] +
+                "\n--------------------------------------\n" +
                 "Enter one of the following commands:\n" +
                 " /mse\n /psnr\n /qi\n /hist\n /all_evals\n /quit\n" +
                 "---------------------------------------\n\n" +
@@ -126,14 +138,14 @@ def main():
             ).lower()
 
         # Handles method of evaluation, quitting, and going back to home
-        if (eval_type == "/mse"):
+        if eval_type == "/mse":
             print("{:<5} | {:<20} | Lower = Better".format("MSE", algorithm.calc_mse()))
-        elif (eval_type == "/psnr"):
+        elif eval_type == "/psnr":
             print("{:<5} | {:<20} | Higher = Better".format("PSNR", algorithm.calc_psnr()))
-        elif (eval_type == "/qi"):
+        elif eval_type == "/qi":
             qi = algorithm.calc_qi()
             print("{:<5} | {:<20} | Higher = Better".format("QI", "INVALID (STDDEV=0)" if qi == INFINITY else qi))
-        elif (eval_type == "/hist"):
+        elif eval_type == "/hist":
             hist_info = algorithm.show_hist()
             print("{:<20} | {:<20}".format("Mean Original", hist_info[0]))
             print("{:<20} | {:<20}".format("Mean Stego", hist_info[1]))
@@ -141,7 +153,7 @@ def main():
             print("{:<20} | {:<20}".format("StdDev Stego", hist_info[3]))
             print("{:<20} | {:<20}".format("MinMax Original", str(hist_info[4])))
             print("{:<20} | {:<20}".format("MinMax Stego", str(hist_info[5])))
-        elif (eval_type == "/all_evals"):
+        elif eval_type == "/all_evals":
             print("\n--==DISTORTION MEASUREMENTS==--")
             print("{:<5} | {:<20} | Lower = Better".format("MSE", algorithm.calc_mse()))
             print("{:<5} | {:<20} | Higher = Better".format("PSNR", algorithm.calc_psnr()))
@@ -156,70 +168,75 @@ def main():
             print("{:<20} | {:<20}".format("StdDev Stego", hist_info[3]))
             print("{:<20} | {:<20}".format("MinMax Original", str(hist_info[4])))
             print("{:<20} | {:<20}".format("MinMax Stego", str(hist_info[5])))
-        elif (eval_type == "/home"):
+        elif eval_type == "/home":
             print("Returning to home.\n")
             main()
-        elif (eval_type == "/quit"):
+        elif eval_type == "/quit":
             print("Quitting...")
             return
 
-    main() # Brings user back to the beginning to do it again
+    main()  # Brings user back to the beginning to do it again
+
 
 # Gets user input of images. Number of images depends on algorithm being used
 def input_image(num_images, image_array):
     # Since only two images can be used at max, throw an error if it tries to use more
-    if (num_images > 2):
+    if num_images > 2:
         raise ValueError("Number of images cannot exceed two.")
 
     # Loops through each image that needs to be added
-    for i in range(0,num_images):
+    for i in range(0, num_images):
         # Gets the path of the image to process
         image_path = input(
             "\n---------------------------------------------------------\n" +
-            "Enter path of image or enter one of the following commands (Image " + str(len(image_array)+1) + "):\n" +
+            "Enter path of image or enter one of the following commands " +
+            "(Image " + str(len(image_array) + 1) + "):\n" +
             " /home\n /quit\n" +
             "---------------------------------------------------------\n\n" +
             "> "
         )
 
         # Input validation for image path
-        while (not os.path.isfile(image_path) and image_path.lower() != "/quit" and image_path.lower() != "/home"):
+        while not os.path.isfile(image_path) and image_path.lower() != "/quit" and image_path.lower() != "/home":
             image_path = input(
                 colors["invalid"] + "Invalid command/path.\n" + colors["normal"] +
                 "\n---------------------------------------------------------\n" +
-                "Enter path of image or enter one of the following commands (Image " + str(len(image_array)+1) + "):\n" +
+                "Enter path of image or enter one of the following commands " +
+                "(Image " + str(len(image_array) + 1) + "):\n" +
                 " /home\n /quit\n" +
                 "---------------------------------------------------------\n\n" +
                 "> "
             )
 
         # Handles quitting and going back to home
-        if (image_path.lower() == "/quit"):
+        if image_path.lower() == "/quit":
             print("Quitting...")
             return
-        elif (image_path.lower() == "/home"):
+        elif image_path.lower() == "/home":
             main()
 
         # Attempts to open the image from the given path
         # Retries image path input on exception
         try:
-            if (num_images > 1 or len(image_array) > 0):
+            if num_images > 1 or len(image_array) > 0:
                 with Image.open(image_path).convert("RGB") as im:
                     im.load()
             else:
                 with Image.open(image_path) as im:
                     im.load()
 
-            if (len(image_array) > 0 and (im.size[0] != image_array[0].size[0] or im.size[1] != image_array[0].size[1])):
+            if len(image_array) > 0 and (im.size[0] != image_array[0].size[0] or im.size[1] != image_array[0].size[1]):
                 raise ValueError
 
             image_array.append(im)
             num_images -= 1
         except:
-            print(colors["invalid"] + "An error occurred. Was the file a PNG/JPEG image? If inputting more than one image, were they the same size?" + colors["normal"])
+            print(colors["invalid"] + "An error occurred. Was the file a PNG/JPEG image?" +
+                  "If inputting more than one image, were they the same size?" + colors["normal"])
             input_image(num_images, image_array)
-    
+
     return image_array
+
 
 if __name__ == "__main__":
     main()
--- src/UniSteg.py
+++ src/UniSteg.py
@@ -1,24 +1,25 @@
 #########################################################
-# UniSteg.py v0.2
+# UniSteg.py v0.3
 # Wesley Jacobs
 #
 # Currently an exploratory program that conceals and
 # extracts message to and from images.
 #########################################################
 
-import sys            # used to exit on error/quit
+import sys                 # used to exit on error/quit
 
 try:
-    from PIL import Image # used for image processing
-    import numpy as np    # used for more optimized arrays
+    from PIL import Image  # used for image processing
+    import numpy as np     # used for more optimized arrays
 except:
     print("Missing a module. Did you run 'pip install -r modules.txt'?")
     sys.exit()
 
+
 class UniSteg:
     # Constructor that sets the image to conceal/extract if provided
     def __init__(self, image=None):
-        if (type(image) == type(None)):
+        if image is None:
             self._image = image
         else:
             self.set_image(image)
@@ -29,39 +30,40 @@ class UniSteg:
             fmt = image.format
         except:
             raise TypeError("Image must be of Image type.")
-        if (fmt != "JPEG" and fmt != "PNG"):
+        if fmt != "JPEG" and fmt != "PNG":
             raise TypeError("Image must be a PNG or JPEG.")
         self._image = image
 
     # Conceals a secret message into an image and saves it in current directory
     def conceal(self):
-        if (type(self._image) == type(None)):
+        if self._image is None:
             raise TypeError("Image is of None type.")
 
         secret_message_string = input("Enter your secret message: ")
 
         # convert string into an array of ASCII values
-        secret_message_byte_array = bytearray(secret_message_string, "ascii")
-        secret_message_binary = np.array([], dtype=int) # used to store the secret message in binary format
+        secret_message_byte_array = bytearray(secret_message_string, "utf-8")
+        secret_message_binary = np.array([], dtype=int)  # used to store the secret message in binary format
 
         # loops through each ascii value
         for byte in secret_message_byte_array:
-            # Adds leading zeros so each character is guaranteed to be 8 bits in length [2:] gets rid of the '0b' before the binary value
+            # Adds leading zeros so each character is guaranteed to be 8 bits in length
+            # [2:] gets rid of the '0b' before the binary value
             for bit in range(8-len(str(bin(byte)[2:]))):
                 secret_message_binary = np.append(secret_message_binary, 0)
             # Adds the byte of data to the secret message binary string
             secret_message_binary = np.concatenate((secret_message_binary, [int(ch) for ch in list(bin(byte)[2:])]))
 
         # Add null character to signify the ending
-        secret_message_binary = np.append(secret_message_binary, [0,0,0,0,0,0,0,0])
+        secret_message_binary = np.append(secret_message_binary, [0] * 8)
 
         # Stores the subtraction mask, which is what turns the original pixel's bits into secret message bits
         subtract_mask = np.array(self._image).flatten()[:len(secret_message_binary)]
 
         # Create subtraction mask based on the secret message
         for i in range(len(subtract_mask)):
-            if (subtract_mask[i] % 2 != secret_message_binary[i]):
-                if (subtract_mask[i]-1 > 0):
+            if subtract_mask[i] % 2 != secret_message_binary[i]:
+                if subtract_mask[i]-1 > 0:
                     subtract_mask[i] = 1
                 else:
                     subtract_mask[i] = -1
@@ -70,14 +72,14 @@ class UniSteg:
 
         # Create the new image from the subtraction mask
         im_flattened = np.array(self._image).flatten()
-        im_stego = np.append(np.subtract(im_flattened[:len(secret_message_binary)], subtract_mask), im_flattened[len(secret_message_binary):]).reshape(np.array(self._image).shape)
+        im_stego = np.append(np.subtract(im_flattened[:len(secret_message_binary)], subtract_mask),
+                             im_flattened[len(secret_message_binary):]).reshape(np.array(self._image).shape)
 
         Image.fromarray(im_stego).save('stego.png')
-        print("Successfully created stegimage.")
 
     # Extracts secret message from image and outputs it to console
     def extract(self):
-        if (type(self._image) == type(None)):
+        if self._image is None:
             raise TypeError("Image is of None type.")
 
         binary_message = ""
@@ -86,22 +88,23 @@ class UniSteg:
         # Stores how many zeros in a row to find null character
         num_zeros = 0
 
-        # Creates a string of least significant bits in the image that correlate to a secret message
+        # Creates a string of the least significant bits in the image that correlate to a secret message
         for i in range(len(im_flattened)):
             binary_message += str(im_flattened[i] % 2)
 
-            if (im_flattened[i] % 2 == 0):
+            if im_flattened[i] % 2 == 0:
                 num_zeros += 1
             else:
                 num_zeros = 0
 
-            # If the end of a byte and found 8 or more zeros (accounting for 0s at the end of previous byte), end the search
+            # If the end of a byte and found 8 or more zeros (accounting for 0s at the end of previous byte),
+            # end the search
             if num_zeros >= 8 and (i + 1) % 8 == 0:
                 break
 
         # Convert to decimal ASCII values
-        binary_message = int(binary_message, 2)
+        binary_message = int(binary_message[:-8], 2)
         # Converts the formatted string into readable bytes
         byte_array = binary_message.to_bytes((binary_message.bit_length() + 7) // 8, "big")
 
-        print("Decoded message: \"" + byte_array.decode("ascii") + "\"")
+        return byte_array.decode("utf-8")
